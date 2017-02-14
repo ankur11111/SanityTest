b9dfb54afa6f8993d4820ab6b51eac9334e9f3e2
ankur <ankur@Ankur.optym.local>
2017-02-14 13:01:04 +0530

    8yup

---
+++ src/SanityTestCases/FirstTest.java
@@ -0,0 +1,27 @@
+package SanityTestCases;
+
+import org.junit.Test;
+
+public class FirstTest {
+
+	@Test
+	public void FirstTest()
+	{
+		System.out.println("First   Test");
+	}
+	@Test
+	public void SecondTest()
+	{
+		System.out.println("Second   Test");
+	}
+	@Test
+	public void ThirdTest()
+	{
+		System.out.println("Third Test");
+	}
+	@Test
+	public void FourthTest()
+	{
+		System.out.println("Fourth Test");
+	}
+}
